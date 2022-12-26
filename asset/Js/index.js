a9a8d9af7039eb81ad82ad2f4b25f26a6ee8e743
Karthik s Kailas <karthikskailas@gmail.com>
2022-12-26 22:21:35 +0530

    modified code

--- asset/Js/index.js
+++ asset/Js/index.js
@@ -1,49 +1,56 @@
-const form = document.querySelector('#searchfrm')
-const imgDiv = document.querySelector('#imagecontainer')
+const values = {
+	imgDiv: document.querySelector("#imagecontainer"),
+	form: document.querySelector("#searchfrm"),
+	cardImg: document.querySelector(".card-img-top"),
+	card: document.querySelector("#main-card"),
+	title: document.querySelector(".card-title"),
+	lang: document.querySelector("#lang"),
+	genres: document.querySelector("#gen"),
+	status: document.querySelector("#sat"),
+	links: document.querySelector(".links"),
+	imgCreate(shows) {
+		for (result of shows) {
+			const files = {
+				image: result.show.image.medium,
+				name: result.show.name,
+				language: result.show.language,
+				genres: result.show.genres,
+				status: result.show.status,
+				link: result.show.url,
+			};
+			if (files.image) {
+				this.title.innerHTML = files.name;
+				this.lang.innerHTML = files.language;
+				this.genres.innerHTML = files.genres;
+				this.status.innerHTML = files.status;
+				this.links.setAttribute("href", files.link);
+				values.cardImg.src = files.image;
+				const clone = this.card.cloneNode(true);
+				this.imgDiv.append(clone);
+				clone.classList.remove("dis-non");
+			}
+		}
+	},
+	removeAllChildNodes(parent) {
+		while (parent.firstChild) {
+			parent.removeChild(parent.firstChild);
+		}
+	},
+};
 
-form.addEventListener('submit', async e => {
-  e.preventDefault()
-  const query = form.elements.yo.value
-  const config = { params: { q: query } }
-  const res = await axios.get('https://api.tvmaze.com/search/shows?q=', config)
-  if (imgDiv.childNodes.length !== 0) {
-    removeAllChildNodes(imgDiv)
-  }
-  setTimeout(() => {
-    imgCreate(res.data)
-  }, 0)
-  form.elements.yo.value = ''
-})
-
-function removeAllChildNodes (parent) {
-  while (parent.firstChild) {
-    parent.removeChild(parent.firstChild)
-  }
-}
-
-const imgCreate = shows => {
-  for (result of shows) {
-    if (result.show.image.medium) {
-      const data = {
-        image: result.show.image.medium,
-        name: result.show.name,
-        language: result.show.language,
-        genres: result.show.genres,
-        status: result.show.status,
-        link: result.show.url
-      }
-      document.querySelector('.card-title').innerHTML = data.name
-      document.querySelector('#lang').innerHTML = data.language
-      document.querySelector('#sat').innerHTML = data.status
-      document.querySelector('#gen').innerHTML = ` ${data.genres}`
-      document.querySelector('.links').setAttribute('href', data.link)
-      console.log(result)
-      const img = document.querySelector('.card-img-top')
-      img.src = data.image
-      const card = document.querySelector('#main-card')
-      const clone = card.cloneNode(true)
-      imgDiv.append(clone)
-      clone.classList.remove('dis-non')
-    }
-  }
-}
+values.form.addEventListener("submit", async (e) => {
+	e.preventDefault();
+	const query = values.form.elements.search.value;
+	const config = { params: { q: query } };
+	const res = await axios.get(
+		"https://api.tvmaze.com/search/shows?q=",
+		config,
+	);
+	if (values.imgDiv.childNodes.length !== 0) {
+		values.removeAllChildNodes(values.imgDiv);
+	}
+	setTimeout(() => {
+		values.imgCreate(res.data);
+	}, 0);
+	values.form.elements.search.value = "";
+});
